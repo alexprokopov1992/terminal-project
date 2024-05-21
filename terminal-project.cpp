436ed16bd243151212f1ca76bc09c41a279b45ca
Alexandr Prokopov <alexprokopov@gmail.com>
2024-05-21 20:15:18 +0300

    add new function create_file_in_folder

--- terminal-project.cpp
+++ terminal-project.cpp
@@ -5,23 +5,27 @@
 using namespace std;
 using namespace std::filesystem;
 
-int main() {
-    path directorypath = "mydirectory";
+void create_file_in_folder(std::string file_name, std::string folder_name, std::string text)
+{
+    path directorypath = folder_name;
     if (!exists(directorypath)) {
         create_directory(directorypath);
         cout << "Directory created: " << directorypath << endl;
     }
 
-    path filepath = directorypath / "my_file.txt";
+    path filepath = directorypath / file_name;
     ofstream file(filepath);
     if (file.is_open()) {
-        file << "Hello, FileSystem!";
+        file << text;
         file.close();
         cout << "File created: " << filepath << endl;
     }
     else {
         cerr << "Failed to create file: " << filepath << endl;
     }
+}
 
+int main() {
+    create_file_in_folder("1.txt", "test", "Good evening");
     return 0;
 }
